38600dd95171b58ce8131affb738282b348f9298
ayoolatech19 <mahmoudomotosho12@gmail.com>
2026-09-17 13:00:07 -0700

    categoriey controller with requests

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 Route::prefix('v1')->group(function () {
     Route::get('/user', function (Request $request) {
@@ -13,4 +14,11 @@ Route::prefix('v1')->group(function () {
     Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->middleware('auth:sanctum') ;
     Route::get('/me', [App\Http\Controllers\AuthController::class, 'me'])->middleware('auth:sanctum') ;
 
-    });
+    Route::get('categories', [CategoryController::class, 'index']);
+
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::post('categories', [CategoryController::class, 'store']);
+    Route::put('categories/{category}', [CategoryController::class, 'update']);
+    Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+});
+});
